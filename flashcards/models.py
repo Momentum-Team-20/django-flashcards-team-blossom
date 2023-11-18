93c8012b9cfeb00518019c2cfd36175991a7430a
freddiebell88 <frederikcwbell@gmail.com>
2023-11-18 11:32:38 -0500

    registered models

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 class User(AbstractUser):
     pass
+
 
 class Deck(models.Model):
     title = models.CharField(max_length=250)
@@ -11,6 +13,7 @@ class Deck(models.Model):
 
     def __str__(self):
         return self.title
+
 
 # Flashcards uses Deck as a foreign key, because many cards can have one deck
 class Flashcard(models.Model):
