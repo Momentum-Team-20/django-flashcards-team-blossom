c3bcae5af16fcf4085098fe5155e3fab43f3c09d
Kelsey Barham <kelseylbarham@gmail.com>
2023-11-18 10:36:41 -0500

    Created models for Deck and for Flashcards where flashcards have a many to one relationship to deck, also I accidentally added a urls.py page thinking I needed it for the models.py but I was mistaken - we'll use it down the road so I left it lol

--- flashcards/models.py
+++ flashcards/models.py
@@ -4,3 +4,19 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Deck(models.Model):
+    title = models.CharField(max_length=250)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+# Flashcards uses Deck as a foreign key, because many cards can have one deck
+class Flashcard(models.Model):
+    question = models.CharField(max_length=500)
+    answer = models.CharField(max_length=500)
+    deck = models.ForeignKey('Deck', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.question
