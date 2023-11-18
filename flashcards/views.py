5d4d7209369f87a13693ae75191b1d4df6924c6f
freddiebell88 <frederikcwbell@gmail.com>
2023-11-18 12:11:12 -0500

    created superuser and build one deck with flashcards, attempting to get a home page

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def deck_list(request):
+    decks = Deck.objects.all()
+    return render(request, 'flashcards/index.html', {'decks': decks})
