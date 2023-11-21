ee31e1687a98a28b37b41815cf3b532c679093af
freddiebell88 <frederikcwbell@gmail.com>
2023-11-21 16:03:14 -0500

    added js for correct/incorrect buttons change color on click event

---
+++ static/main.js
@@ -0,0 +1,24 @@
+let correctAnswerButtons = document.querySelectorAll('#correct')
+let incorrectAnswerButtons = document.querySelectorAll('#incorrect')
+
+// document.querySelector('button').addEventListener('click', function() {
+//     document.querySelector()
+// } )
+
+// function changeColor(correctAnswer) {
+//     correctAnswer.addEventListener('click', () => {
+//         correctAnswer.style.color='green';
+//     })
+// }
+
+for ( let button of correctAnswerButtons) {
+    button.addEventListener('click', (event) => {
+        button.style.color = 'green'
+    })
+}
+
+for ( let button of incorrectAnswerButtons) {
+    button.addEventListener('click', (event) => {
+        button.style.color = 'red'
+    })
+}
